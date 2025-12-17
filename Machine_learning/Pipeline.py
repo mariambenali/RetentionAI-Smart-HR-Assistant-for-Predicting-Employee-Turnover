d4a205c40dd5fef796a9aba22689cb4a95ffb188
mariambenali <miriam.bena@gmail.com>
2025-12-17 10:18:19 +0100

    add EDA and save the model at file.joblib

---
+++ Machine_learning/Pipeline.py
@@ -0,0 +1,65 @@
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report
+import pandas as pd
+
+
+
+def preparing(data):
+    df = pd.read_csv('/data/dt.csv')
+
+    return df
+
+print(a)
+
+
+'''
+
+#preparation des donnés
+
+le= LabelEncoder()
+y = le.fit_transform(y)
+print(len(y))
+
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    x,y,
+    test_size =0.2,
+    random_state= 42,)
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), num_cols),
+        ('cat', OneHotEncoder(drop='first'), cat_cols)
+    ]
+)
+models ={
+    "LogisticRegression": LogisticRegression(max_iter=1000),
+    "RandomForest": RandomForestClassifier(random_state=42)
+
+}
+
+param_grids = {
+    "LogisticRegression": {
+        'model__C': [0.01, 0.1, 1, 10]
+    },
+    "RandomForest": {
+        'model__n_estimators': [200, 500],
+        'model__max_depth': [4, 5, 6, 7, 8],
+        'model__criterion':['gini', 'entropy']
+    }
+}
+model = LogisticRegression()
+
+pipeline = Pipeline([
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+
+pipeline.fit(X_train,y_train)'''
