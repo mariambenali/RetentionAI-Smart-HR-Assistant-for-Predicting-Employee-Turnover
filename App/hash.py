65f4fd62325442818ad9dd3187dbb2169e9a57bb
mariambenali <miriam.bena@gmail.com>
2025-12-18 00:06:09 +0100

    add token to endpoint login and verified hashed pwd

--- App/hash.py
+++ App/hash.py
@@ -1,25 +1,23 @@
 import bcrypt
 
 
-password = "1236436"
-
-def hashed_password(password):
-    password_bytes = password.encode("utf-8")
-
-    salt = bcrypt.gensalt()  
-
-    hashed_password_bytes = bcrypt.hashpw(password_bytes, salt) 
 
 
-    return hashed_password_bytes
-	
-hashed_password_bytes= hashed_password(password)
-
-print(hashed_password_bytes)
+def hashed_password(password:str):
+    password_bytes = password.encode('utf-8')
 
 
-def verify_password(plain_password:str, hashed_password:str) -> bool:
+    salt = bcrypt.gensalt()
+
+    hashed_password_bytes = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password_bytes.decode('utf-8')
+
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         plain_password.encode("utf-8"),
         hashed_password.encode("utf-8")
     )
+    
--- App/main.py
+++ App/main.py
@@ -2,19 +2,23 @@ from .database import Base, engine, get_db
 from .schema import UserCreate, UserLogin, Prediction
 from .models import User, Employee
 from fastapi import FastAPI, Depends, HTTPException
-from .hash import hashed_password
+from .hash import hashed_password, verify_password
+from .security import SECRET_KEY, ALGORITHM
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.orm import Session
 from datetime import date
+from jose import jwt
 
 
 
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+type_token= HTTPBearer()
 
 @app.post("/register")
 def register(user:UserCreate, db: Session = Depends(get_db)):
-    hash_password = hashed_password(user.password)
+    hash_password = hashed_password(user.password) 
     new_user=User (
         username= user.username,
         password= hash_password,
@@ -27,14 +31,26 @@ def register(user:UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/login")
+@app.post("/login")
 def login(user:UserLogin, db: Session = Depends(get_db)):
-    user_login = db.query(User).filter(User.username == user.username)
+    user_login = db.query(User).filter(User.username == user.username).first()
 
     if not user_login:
         raise HTTPException(status_code = 404, detail="User not found")
-    elif not verify_password(user.password, user_login.hashed_password)
+    elif not verify_password(user.password, user_login.password):
+        raise HTTPException(status_code=401, detail="Invalid password")
+    
+    payload = {"sub": user_login.username}
+    token = jwt.encode(payload,SECRET_KEY,algorithm=ALGORITHM)
 
-    return user_login
+    return token
 
     
+
+@app.get("/token")
+def get_token(token:HTTPAuthorizationCredentials = Depends(type_token)):
+    try:
+        decoded = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token")
